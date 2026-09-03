f05c601bf054a8dcc220f8549a95c7c550c5551d
unknown <jar.cofre@duocuc.cl>
2026-09-02 20:51:14 -0400

    login de mi pagina

---
+++ login.js
@@ -0,0 +1,22 @@
+function ingresar(){
+    let correo = document.getElementById("Correo").value;
+    let contrasena = document.getElementById("Contrasena").value;
+
+    if (correo === "" || clave === "") {
+        alert("Debe completar todos los campos");
+        return;
+    }   
+
+    let formatoCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!formatoCorreo.test(correo)) {
+        alert("El correo no es válido");
+        return;
+    }
+    if(clave.length !==4) {
+        alert("La contraseña debe tener 4 caracteres")
+        return;
+    }
+
+
+}
