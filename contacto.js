8e7bcafcd88dcec571b8490c1868a5168ea64dab
simon <simonpanita18@gmail.com>
2026-09-07 22:13:24 -0300

    areglando para que manden buenos msj

--- contacto.js
+++ contacto.js
@@ -12,13 +12,13 @@ formulario.addEventListener("submit", function(event) {
         return;
     }
 
-    if(nombre.length < 100) {
-        alert("El nombre debe tener al menos 100 caracteres");
+    if(nombre.length < 10) {
+        alert("El nombre debe tener al menos 10 caracteres");
         return;
     }
 
-    if(mensaje.length<500){
-        alert("El mensaje debe tener al menos 500 caracteres");
+    if(mensaje.length< 10){
+        alert("El mensaje debe tener al menos 10 caracteres");
     }
 
     let formatoCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -32,4 +32,9 @@ formulario.addEventListener("submit", function(event) {
         alert("El mensaje no puede superar los 500 caracteres");
         return;
     }
+    console.log("Nombre: " + nombre + "\nCorreo: " + correo + "\nMensaje: " + mensaje);
+    localStorage.setItem("nombre", nombre);
+    localStorage.setItem("correo", correo);
+    localStorage.setItem("mensaje", mensaje);    
+    alert("Formulario enviado correctamente.");
 });
